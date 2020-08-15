7d929208e608c259f83599ec9f5c6f98a18622a1
Francisco Araujo <franciscogui29@gmail.com>
2020-08-14 23:38:20 -0300

    "refatorando codigo"

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,34 @@
-{
-    var redRange = document.querySelector('#redRange')
-    var redText = document.querySelector('#redText')
-    var redMixed = document.querySelector('#redMixed')
-    var boxRed = document.querySelector('#boxRed')
-    var boxMix = document.querySelector('#boxMix')
+let redRange = document.querySelector('#redRange');
+let redText = document.querySelector('#redText');
+let redMixed = document.querySelector('#redMixed');
+let boxRed = document.querySelector('#boxRed');
+let blueRange = document.querySelector('#blueRange');
+let blueText = document.querySelector('#blueText');
+let blueMixed = document.querySelector('#blueMixed');
+let boxBlue = document.querySelector('#boxBlue');
+let greenRange = document.querySelector('#greenRange');
+let greenText = document.querySelector('#greenText');
+let greenMixed = document.querySelector('#greenMixed');
+let boxGreen = document.querySelector('#boxGreen');
+let boxMix = document.querySelector('#boxMix');
 
-    redRange.addEventListener('mousemove', function mouseModify(){
-        redText.value = this.value
-        redMixed.value = this.value
-        boxRed.style.backgroundColor = `rgb(${this.value}, ${0}, ${0})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
+redRange.addEventListener('input', function mouseModify() {
+	redText.value = this.value;
+	redMixed.value = this.value;
+	boxRed.style.backgroundColor = `rgb(${this.value}, ${0}, ${0})`;
+	boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
+});
 
-    redRange.addEventListener('keydown', function keyModify(){
-        redText.value = this.value
-        redMixed.value = this.value
-        boxRed.style.backgroundColor = `rgb(${this.value}, ${0}, ${0})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
-}
+greenRange.addEventListener('input', function mouseModify() {
+	greenText.value = this.value;
+	greenMixed.value = this.value;
+	boxGreen.style.backgroundColor = `rgb(${0}, ${this.value}, ${0})`;
+	boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
+});
 
-{
-    var greenRange = document.querySelector('#greenRange')
-    var greenText = document.querySelector('#greenText')
-    var greenMixed = document.querySelector('#greenMixed')
-    var boxGreen = document.querySelector('#boxGreen')
-    var boxMix = document.querySelector('#boxMix')
-
-    greenRange.addEventListener('mousemove', function mouseModify(){
-        greenText.value = this.value
-        greenMixed.value = this.value
-        boxGreen.style.backgroundColor = `rgb(${0}, ${this.value}, ${0})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
-
-    greenRange.addEventListener('keydown', function keyModify(){
-        greenText.value = this.value
-        greenMixed.value = this.value
-        boxGreen.style.backgroundColor = `rgb(${0}, ${this.value}, ${0})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
-}
-
-{
-    var blueRange = document.querySelector('#blueRange')
-    var blueText = document.querySelector('#blueText')
-    var blueMixed = document.querySelector('#blueMixed')
-    var boxBlue = document.querySelector('#boxBlue')
-    var boxMix = document.querySelector('#boxMix')
-
-    blueRange.addEventListener('mousemove', function mouseModify(){
-        blueText.value = this.value
-        blueMixed.value = this.value
-        boxBlue.style.backgroundColor = `rgb(${0}, ${0}, ${this.value})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
-
-    blueRange.addEventListener('keydown', function keyModify(){
-        blueText.value = this.value
-        blueMixed.value = this.value
-        boxBlue.style.backgroundColor = `rgb(${0}, ${0}, ${this.value})`;
-        boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
-    })
-}
+blueRange.addEventListener('input', function mouseModify() {
+	blueText.value = this.value;
+	blueMixed.value = this.value;
+	boxBlue.style.backgroundColor = `rgb(${0}, ${0}, ${this.value})`;
+	boxMix.style.backgroundColor = `rgb(${redRange.value}, ${greenRange.value}, ${blueRange.value})`;
+});
